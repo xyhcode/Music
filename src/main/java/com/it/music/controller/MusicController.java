02f7917374e89eb528d34cd7dfeccf155292b807
羡羡 <xyhghy@outlook.com>
2021-11-18 21:46:02 +0800

    精选视频

--- src/main/java/com/it/music/controller/MusicController.java
+++ src/main/java/com/it/music/controller/MusicController.java
@@ -1,10 +1,16 @@
 package com.it.music.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.it.music.service.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
@@ -51,4 +57,31 @@ public class MusicController {
         mp.put("feli",felis);
         return "fontdesk/index";
     }
+
+    /**
+     * 视频分页
+     * @param map
+     * @return
+     */
+    @GetMapping("/morvide")
+    public String morvid(ModelMap map){
+        PageHelper.startPage(1,20);
+        List lis=fese.sefe();
+        PageInfo pa=new PageInfo(lis,10);
+        map.put("pavo",pa);
+        return "fontdesk/morevide";
+    }
+    @RequestMapping(path = "/showvide/{curr}",method = RequestMethod.GET)
+    public String show(ModelMap map, @PathVariable("curr") int curr){
+        if(curr==0){
+            curr=1;
+        }
+        PageHelper.startPage(curr,20);
+        List lis=fese.sefe();
+        PageInfo pa=new PageInfo(lis,10);
+        map.put("pavo",pa);
+        return "fontdesk/morevide";
+    }
+
+
 }
--- src/main/java/com/it/music/entity/SongList.java
+++ src/main/java/com/it/music/entity/SongList.java
@@ -6,7 +6,9 @@ package com.it.music.entity;
 public class SongList {
     public int solid;
     public String soltitle;
+    public String solintro;
     public String solimg;
+    public String sotid;
     public String soid;
     public int usid;
 
@@ -26,12 +28,28 @@ public class SongList {
         this.soltitle = soltitle;
     }
 
+    public String getSolintro() {
+        return solintro;
+    }
+
+    public void setSolintro(String solintro) {
+        this.solintro = solintro;
+    }
+
     public String getSolimg() {
         return solimg;
     }
 
     public void setSolimg(String solimg) {
         this.solimg = solimg;
+    }
+
+    public String getSotid() {
+        return sotid;
+    }
+
+    public void setSotid(String sotid) {
+        this.sotid = sotid;
     }
 
     public String getSoid() {
@@ -50,20 +68,21 @@ public class SongList {
         this.usid = usid;
     }
 
-    public SongList() {
-    }
-
-    public SongList(String soltitle, String solimg, String soid, int usid) {
+    public SongList(String soltitle, String solintro, String solimg, String sotid, String soid, int usid) {
         this.soltitle = soltitle;
+        this.solintro = solintro;
         this.solimg = solimg;
+        this.sotid = sotid;
         this.soid = soid;
         this.usid = usid;
     }
 
-    public SongList(int solid, String soltitle, String solimg, String soid, int usid) {
+    public SongList(int solid, String soltitle, String solintro, String solimg, String sotid, String soid, int usid) {
         this.solid = solid;
         this.soltitle = soltitle;
+        this.solintro = solintro;
         this.solimg = solimg;
+        this.sotid = sotid;
         this.soid = soid;
         this.usid = usid;
     }
@@ -73,7 +92,9 @@ public class SongList {
         return "\nSongList{" +
                 "solid=" + solid +
                 ", soltitle='" + soltitle + '\'' +
+                ", solintro='" + solintro + '\'' +
                 ", solimg='" + solimg + '\'' +
+                ", sotid='" + sotid + '\'' +
                 ", soid='" + soid + '\'' +
                 ", usid=" + usid +
                 '}';
