228cf73e0b2d8a08035fba6c5ebbdc8947f07e2d
2513119125@qq.com <2513119125@qq.com>
2021-11-21 13:39:51 +0800

    显示登录用户

--- src/main/java/com/it/music/controller/QLoginController.java
+++ src/main/java/com/it/music/controller/QLoginController.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import javax.servlet.http.HttpServletRequest;
+
 /**
  * @author lingjing
  */
 @Controller
-@SessionAttributes({"user"})
-@CrossOrigin
 public class QLoginController {
 
     @Autowired
@@ -29,19 +29,17 @@ public class QLoginController {
 
     @ResponseBody
     @RequestMapping("/login")
-    public JsonResult login(ModelMap mm,User us){
+    public JsonResult login(HttpServletRequest request, User us){
 //        System.out.println("手机号："+us.getPhone());
 //        System.out.println("密码："+us.getPassword());
         User user = userService.login(us);
         JsonResult jr = null;
         if(user != null){
-            mm.put("user",user);
+            request.getSession().setAttribute("user",user);
             jr = new JsonResult(200,"登录成功！");
         }else{
             jr = new JsonResult(500,"登录失败！");
         }
-
-        mm.clear();
         return jr;
     }
 
