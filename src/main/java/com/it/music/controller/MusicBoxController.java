e73c5f3238a39b04e38fcb738882df27de769286
十一 <fhd3624251@outlook.com>
2021-11-22 14:03:05 +0800

    音乐盒 播放、上一首、下一首

--- src/main/java/com/it/music/controller/MusicBoxController.java
+++ src/main/java/com/it/music/controller/MusicBoxController.java
@@ -1,7 +1,9 @@
 package com.it.music.controller;
 
+import com.it.music.entity.Song;
 import com.it.music.entity.SongSing;
 import com.it.music.service.PlayListService;
+import com.it.music.tools.FindSubscript;
 import com.it.music.tools.JsonUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -29,6 +31,8 @@ public class MusicBoxController {
         return "fontdesk/musicbox";
     }
 
+
+    //播放
     @GetMapping("play/{soid}")
     public String play(@PathVariable("soid") int soid,ModelMap map) throws IOException {
         List list=playListService.getSongList(uid);//播放列表歌曲
@@ -42,7 +46,54 @@ public class MusicBoxController {
         return "fontdesk/musicbox";
     }
 
+    //上一首
+    @GetMapping("prev/{soid}")
+    public String prev(@PathVariable("soid") int soid,ModelMap map) throws IOException {
+        List list=playListService.getSongList(uid);//播放列表歌曲
+        int[] idlist=new int[list.size()];
+        for (int i=0;i<list.size();i++){
+            SongSing z=(SongSing)list.get(i);
+            idlist[i]=z.soid;
+        }
+        int xb= FindSubscript.binarySearch(idlist,soid);
+        int pxb=xb-1;
+        if (pxb<0){
+            pxb=idlist.length-1;
+        }
+        int sid=idlist[pxb];
+        System.out.println(sid);
+        map.put("listInfo",list);
+        SongSing s=playListService.getSong(sid);
+        map.put("playInfo",s);//播放歌曲信息
+        String lyric= JsonUtil.getjson(s.getLyrics());
+        map.put("lyric",lyric);
+        return "fontdesk/musicbox";
+    }
 
+
+    //下一首
+    @GetMapping("next/{soid}")
+    public String next(@PathVariable("soid") int soid,ModelMap map) throws IOException {
+        List list=playListService.getSongList(uid);//播放列表歌曲
+        int[] idlist=new int[list.size()];
+        for (int i=0;i<list.size();i++){
+            SongSing z=(SongSing)list.get(i);
+            idlist[i]=z.soid;
+        }
+        int xb= FindSubscript.binarySearch(idlist,soid);
+        int pxb=xb+1;
+        if (pxb>=idlist.length){
+            pxb=0;
+        }
+        int sid=idlist[pxb];
+        System.out.println(sid);
+        map.put("listInfo",list);
+        SongSing s=playListService.getSong(sid);
+        map.put("playInfo",s);//播放歌曲信息
+        String lyric= JsonUtil.getjson(s.getLyrics());
+        map.put("lyric",lyric);
+        return "fontdesk/musicbox";
+    }
 
 
 }
---
+++ src/main/java/com/it/music/tools/FindSubscript.java
@@ -0,0 +1,24 @@
+package com.it.music.tools;
+
+/**
+ * 查询下标
+ */
+public class FindSubscript {
+
+    public static int binarySearch(int[] array,int target){
+        int left=0;
+        int right=array.length-1;
+        int mid;
+        while(left<=right){
+            mid=left+(right-left)/2;//中间位置
+            if(array[mid]==target){
+                return mid;
+            }else if(target<array[mid]){//如果目标值小于当前Mid下标位置的值 则在前半区查找
+                right=mid-1;
+            }else{//反之在后半区查找
+                left=mid+1;
+            }
+        }
+        return -1;
+    }
+}
